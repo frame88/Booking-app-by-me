85b7559bdad9cace958a0061cfa2f51a3844d993
frame88 <fleuzzi97@gmail.com>
2022-09-12 12:10:05 +0200

    Creazione progetto, gestione router e impaginazione layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/login/login.component';
+import { CartComponent } from './features/cart/cart.component';
+import { SearchComponent } from './features/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CartComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
